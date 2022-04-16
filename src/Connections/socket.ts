2a5a4b2da4313ec9049089a479a825fb6327df5e
ChrisIT9 <cri-9999@live.it>
2022-04-16 21:05:59 +0200

    server port

--- src/Connections/socket.ts
+++ src/Connections/socket.ts
@@ -1,6 +1,6 @@
 import { Server } from 'socket.io';
 
-const io = new Server(Number(process.env.SOCKET_PORT), {
+const io = new Server(Number(process.env.PORT), {
 	pingInterval: Number(process.env.PING_INTERVAL),
 	pingTimeout: Number(process.env.PING_TIMEOUT),
 	transports: ['websocket']
